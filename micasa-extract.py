5745839a74553420241707c2d886c5af50f04ea6
Hannah Zafar <hannah.zafar@nasa.gov>
2024-10-22 17:21:36 -0400

    initial commit

--- micasa-extract.py
+++ micasa-extract.py
@@ -16,13 +16,32 @@ def get_single_match(pattern):
     else:
         raise ValueError(f"Multiple matches found: {matches}")
 
+site_ID = # argparse
+
+filepath = 'ameriflux-data/'
+meta_file = filepath + 'AmeriFlux-site-search-results-202410071335.tsv'
+ameriflux_meta = pd.read_csv(meta_file, sep='\t')
+site_meta = ameriflux_meta.loc[ameriflux_meta['Site ID'] == site_ID]
+
+
+site_file = get_single_match(filepath + 'AMF_' + site_ID + 
+                            '_FLUXNET_SUBSET_*/AMF_' + site_ID + '_FLUXNET_SUBSET_HH_*.csv')
+fluxnet_sel = pd.read_csv(site_file)
+# select subset of columns + create datetime index
+fluxnet_sel_simple = fluxnet_sel[['TIMESTAMP_START','TIMESTAMP_END', 'NEE_VUT_REF']]
+fluxnet_sel_simple .index = pd.to_datetime(fluxnet_sel_simple ['TIMESTAMP_START'],format='%Y%m%d%H%M')
+fluxnet_sel_simple.index.names = ['time']
+
+# Create a list of unique dates from the site
+time = fluxnet_sel_final.time.to_index()
+dates_unique = list({dt.date() for dt in time})
+dates_unique.sort()
 
 # Extract micasa data
 data_path = 'micasa-data/daily-0.1deg-final/holding/3hrly/'
 path_list = []
 
-for date in dates_unique[:500]: #only partial load for speed
-# for date in dates_unique:
+for date in dates_unique:
     f_year = str(date.year)
     f_month = f"{date.month:02}"
     filename = 'MiCASA_v1_flux_*' + date.strftime('%Y%m%d') + '.nc4'
