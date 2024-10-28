f33f4beb05b47877e222d783fa4d81babe12badb
Hannah Zafar <hannah.zafar@nasa.gov>
2024-10-28 13:33:33 -0400

    Build script

--- micasa-extract.py
+++ micasa-extract.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
 # Extract micasa data as csv for plotting
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import xarray as xr
+import argparse
+import sys
 
 
 ######### functions ############
@@ -16,14 +19,21 @@ def get_single_match(pattern):
     else:
         raise ValueError(f"Multiple matches found: {matches}")
 
-site_ID = # argparse
+# Input site ID as parse argument
+parser = argparse.ArgumentParser(description='User-specified parameters')
+parser.add_argument('site_ID', type=str,
+                     help='FluxNet/AmeriFLUX Site Identifier (XX-XXX)')
+args = parser.parse_args()
+site_ID = args.site_ID
+
 
 filepath = 'ameriflux-data/'
 meta_file = filepath + 'AmeriFlux-site-search-results-202410071335.tsv'
 ameriflux_meta = pd.read_csv(meta_file, sep='\t')
 site_meta = ameriflux_meta.loc[ameriflux_meta['Site ID'] == site_ID]
-
-
+site_lat, site_lon = site_meta['Latitude (degrees)'].values, site_meta['Longitude (degrees)'].values
+print(site_lat, site_lon)
+sys.exit()
 site_file = get_single_match(filepath + 'AMF_' + site_ID + 
                             '_FLUXNET_SUBSET_*/AMF_' + site_ID + '_FLUXNET_SUBSET_HH_*.csv')
 fluxnet_sel = pd.read_csv(site_file)
@@ -48,6 +58,10 @@ for date in dates_unique:
     filepath = get_single_match(os.path.join(data_path,f_year,f_month,filename))
     path_list.append(filepath)
 
-
+# open all paths
+ds = xr.open_mfdataset(path_list)['NEE']
+# Select grid closest to selected site
+ds_subset = ds.sel(lon=site_lon, lat=site_lat, method='nearest')
+ds_subset
 
 
