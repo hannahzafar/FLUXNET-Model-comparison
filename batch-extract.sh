53341ad2226f3d5a942134aec37841606a54ec6e
Hannah Zafar <hannah.zafar@nasa.gov>
2024-11-21 15:53:09 -0500

    Update batch reqs

--- batch-extract.sh
+++ batch-extract.sh
@@ -2,11 +2,10 @@
 #SBATCH --account=s1460
 #SBATCH --time=02:00:00
 
+module purge
 module load anaconda
-cd $HOME
 conda activate micasa
 
-cd $NOBACKUP/ghgc/micasa/AmeriFlux-analysis
-./data-extract.py US-Bar
+$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/data-extract.py US-Ne1 HH
 
 conda deactivate
