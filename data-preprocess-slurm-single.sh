e10fa7faa5b8c80999fe5a942a1e618fd1011731
Hannah Zafar <hannah.zafar@nasa.gov>
2025-04-08 13:17:39 -0400

    Update description

--- data-preprocess-slurm-single.sh
+++ data-preprocess-slurm-single.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --account=s1460
 #SBATCH --time=1:30:00
+# Preprocess a single site on compute node
 
 module purge
 module load python/GEOSpyD/Min24.4.0-0_py3.12
--- mp-controller-preprocess-slurm.sh
+++ mp-controller-preprocess-slurm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --account=s1460
 #SBATCH --time=1:30:00
-
+# Preprocess all sites via parallel processing on a compute node
 # Arg1 = 1-5 groups in list
 
 module purge
