c53182fe5d1148bc4d239163c7bbbbbacb77f5e5
Hannah Zafar <hannah.zafar@nasa.gov>
2025-04-09 17:17:36 -0400

    Update SLURM reqs

--- mp-controller-preprocess-slurm.sh
+++ mp-controller-preprocess-slurm.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=multi_script_run
+#SBATCH --output=slurm-%j.out      # Capture both stdout and stderr
+#SBATCH --error=slurm-%j.out       # Same log file for errors
 #SBATCH --account=s1460
 #SBATCH --time=1:30:00
+
 # Preprocess all sites via parallel processing on a compute node
 # Arg1 = 1-5 groups in list
 
