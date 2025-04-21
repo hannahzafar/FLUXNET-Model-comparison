03e630606dd16755a486879c737969744b25cfb7
Hannah Zafar <hannah.zafar@nasa.gov>
2025-04-21 13:04:41 -0400

    Modify to input subbatch number interactively

--- mp-controller-preprocess.sh
+++ mp-controller-preprocess.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-# Preprocess single site via parallel processing on a compute node
-# Arg1 = 1-5 groups in list
+# Interactive bash parallel preprocessing (running one subbatch per run)
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <subbatch number>"
+    exit 1
+fi
 
 module purge
 module load python/GEOSpyD/Min24.4.0-0_py3.12
 
 
-$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py 5
+$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py $1
