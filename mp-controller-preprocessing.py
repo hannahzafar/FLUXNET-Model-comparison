5b4c57bb50ea67bd95cf5bb0752fae6d6c9aa4d0
Hannah Zafar <hannah.zafar@nasa.gov>
2025-04-22 17:44:24 -0400

    Reorganize function/main setup to coding standards

--- mp-controller-preprocessing.py
+++ mp-controller-preprocessing.py
@@ -5,23 +5,14 @@ import numpy as np
 import pandas as pd
 import subprocess
 import multiprocessing
-import argparse
-import sys
-import os
+from functools import partial
 
-# Import list of all sites
-amer_filepath = 'ameriflux-data/'
-meta_file = amer_filepath + 'AmeriFlux-site-search-results-202410071335.tsv'
-ameriflux_meta = pd.read_csv(meta_file, sep='\t')
-fluxnet_meta = ameriflux_meta.loc[ameriflux_meta['AmeriFlux FLUXNET Data'] == 'Yes'] #use FLUXNET only
-fluxnet_list = fluxnet_meta['Site ID'].to_list()
-
-# Testing
-# fluxnet_list = ['US-A32', 'AR-TF1']
-# Function to run script within script
-def run_script(arg_list): 
-    script = "data-preprocessing.py"
-    cmd = ["python", script, arg_list]
+def run_py_script(script, arg): 
+    '''
+    This function runs a python script with one argument via subprocess
+    and returns a subprocess error if it occurs
+    '''
+    cmd = ["python", script, arg]
     try:
         result = subprocess.run(cmd, capture_output=True, text=True, check=True) # check=True will crash if error
         print(result.stdout.strip())
@@ -30,16 +21,28 @@ def run_script(arg_list):
         print(f"Error running command: {' '.join(cmd)}")
         print("STDOUT:", e.stdout)
         print("STDERR:", e.stderr)
-        return f"FAILED: {arg_list}"
+        return f"FAILED: {arg}"
 
-# Function to run scripts in parallel using Pool
-def run_in_parallel(arg_list):
+def run_in_parallel(script, arg_list):
+    '''
+    This function runs run_py_script over a list of input arguments via multiprocessing
+    '''
+    run_script_with_arg = partial(run_py_script, script) # create a partial function to input script to
+
     with multiprocessing.Pool() as pool: # should be fine to not specify pool size as long as no mem issues
-        results = pool.map(run_script, arg_list) # Use map to run `run_script` across the arg_list
-    
+        results = pool.map(run_script_with_arg,arg_list) 
     return results
 
-# Run the script in parallel (interactive safe)
+
 if __name__ == "__main__":
-    results = run_in_parallel(fluxnet_list)
-    #print(results)
+    # Import/format the list of paths
+    amer_filepath = 'ameriflux-data/'
+    meta_file = amer_filepath + 'AmeriFlux-site-search-results-202410071335.tsv'
+    ameriflux_meta = pd.read_csv(meta_file, sep='\t')
+    fluxnet_meta = ameriflux_meta.loc[ameriflux_meta['AmeriFlux FLUXNET Data'] == 'Yes'] #use FLUXNET only
+    fluxnet_list = fluxnet_meta['Site ID'].to_list()
+    
+    script = "data-preprocessing.py"
+
+    # Initialize run
+    results = run_in_parallel(script, fluxnet_list)
