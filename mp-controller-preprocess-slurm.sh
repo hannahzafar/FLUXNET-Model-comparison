882d5d9296510cf897d346fa6ccc078c83f9199d
Hannah Zafar <hannah.zafar@nasa.gov>
2025-04-22 16:44:19 -0400

    Remove batch running (unnecessary) and legacy notation

--- mp-controller-preprocess-slurm.sh
+++ mp-controller-preprocess-slurm.sh
@@ -8,14 +8,9 @@
 #SBATCH --time=2:30:00
 
 # Preprocess all sites via parallel processing on a compute node
-# Arg1 = 1-5 groups in list
 
 module purge
 module load python/GEOSpyD/Min24.4.0-0_py3.12
 
 
-$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py 1
-$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py 2
-$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py 3
-$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py 4
-$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py 5
+$NOBACKUP/ghgc/micasa/AmeriFlux-analysis/mp-controller-preprocessing.py
--- mp-controller-preprocessing.py
+++ mp-controller-preprocessing.py
@@ -9,12 +9,6 @@ import argparse
 import sys
 import os
 
-# Parse input args
-parser = argparse.ArgumentParser(description='Subbatch number')
-parser.add_argument('subbatch', type=int)
-args = parser.parse_args()
-subbatch = args.subbatch
-
 # Import list of all sites
 amer_filepath = 'ameriflux-data/'
 meta_file = amer_filepath + 'AmeriFlux-site-search-results-202410071335.tsv'
@@ -24,19 +18,6 @@ fluxnet_list = fluxnet_meta['Site ID'].to_list()
 
 # Testing
 # fluxnet_list = ['US-A32', 'AR-TF1']
-
-# Fluxnet_list is 196 items, split into sublists for multiple smaller sbatch runs
-# Function to split list into semi-equal sized n-number of groups
-def split_into_groups(list, n):
-    k, m = divmod(len(list), n)
-    return [list[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)]
-
-# Let's do 5 batch jobs that loop through fluxnet_list
-fluxnet_groups = split_into_groups(fluxnet_list,5)
-
-# Pick the one we want via sbatch input
-fluxnet_sel = fluxnet_groups[subbatch-1]
-
 # Function to run script within script
 def run_script(arg_list): 
     script = "data-preprocessing.py"
@@ -68,8 +49,7 @@ def run_in_parallel(arg_list):
     
     return results
 
-
 # Run the script in parallel (interactive safe)
 if __name__ == "__main__":
-    results = run_in_parallel(fluxnet_sel)
+    results = run_in_parallel(fluxnet_list)
     #print(results)
