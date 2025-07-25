6e4a09cf9d4dd1c10acb519eae9c48102a35cb3e
Hannah Zafar <hannah.zafar@nasa.gov>
2025-07-25 16:53:20 -0400

    LSP formatting

--- plotting/plots-generator.py
+++ plotting/plots-generator.py
@@ -10,22 +10,23 @@ import glob
 import os
 import sys
 
+
 ######### functions ############
 # Define a single match file location function
 def get_single_match(pattern):
     """
     Find exactly one file that matches the glob pattern.
-    
+
     Uses glob.glob to find files matching the given pattern. Returns the single
     match if exactly one is found. Raises ValueError if no matches or multiple
     matches are found.
-    
+
     Args:
         pattern (str): A glob pattern to search for files
-        
+
     Returns:
         str: Path to the single matched file
-        
+
     Raises:
         ValueError: If zero or multiple matches are found
     """
@@ -36,6 +37,7 @@ def get_single_match(pattern):
         raise ValueError("No matches found")
     else:
         raise ValueError(f"Multiple matches found: {matches}")
+
 
 def replace_outliers_with_nan(df, column):
     """Replaces outliers in a DataFrame column with NaN.
@@ -54,29 +56,33 @@ def replace_outliers_with_nan(df, column):
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
 
-    df[column] = df[column].mask((df[column] < lower_bound) | (df[column] > upper_bound), np.nan)
+    df[column] = df[column].mask(
+        (df[column] < lower_bound) | (df[column] > upper_bound), np.nan
+    )
     return df
-    
+
+
 ######### input arguments ############
 # Input site ID
-parser = argparse.ArgumentParser(description='User-specified parameters')
-parser.add_argument('site_ID', type=str,
-                     help='FluxNet/AmeriFLUX Site Identifier (XX-XXX)')
+parser = argparse.ArgumentParser(description="User-specified parameters")
+parser.add_argument(
+    "site_ID", type=str, help="FluxNet/AmeriFLUX Site Identifier (XX-XXX)"
+)
 args = parser.parse_args()
 site_ID = args.site_ID
 
 # Define misc variables
-amer_filepath = '../ameriflux-data/'
-mic_filepath = 'preprocess-data/intermediates/'
-timedelta = 'DD'
+amer_filepath = "../ameriflux-data/"
+mic_filepath = "preprocess-data/intermediates/"
+timedelta = "DD"
 
 
 ############## Check if run is necessary #############
 # Check if output file for the site already exists (quits if so)
-output_dir = 'plots'
-output_filename = f'{site_ID}_NEE_NPP.png'
+output_dir = "plots"
+output_filename = f"{site_ID}_NEE_NPP.png"
 output_path = os.path.join(output_dir, output_filename)
-    
+
 # If the file exists, exit the script
 if os.path.exists(output_path):
     print(f"File for site {site_ID} already exists: {output_path}. Exiting.")
@@ -84,19 +90,39 @@ if os.path.exists(output_path):
 
 #################### Import Flux Data ##############################
 # Import site metadata csv
-meta_file = amer_filepath + 'AmeriFlux-site-search-results-202410071335.tsv'
-ameriflux_meta = pd.read_csv(meta_file, sep='\t')
-fluxnet_meta = ameriflux_meta.loc[ameriflux_meta['AmeriFlux FLUXNET Data'] == 'Yes'] #use FLUXNET only
+meta_file = amer_filepath + "AmeriFlux-site-search-results-202410071335.tsv"
+ameriflux_meta = pd.read_csv(meta_file, sep="\t")
+fluxnet_meta = ameriflux_meta.loc[
+    ameriflux_meta["AmeriFlux FLUXNET Data"] == "Yes"
+]  # use FLUXNET only
 
 # Import selected site daily subset data
-site_lat = fluxnet_meta.loc[fluxnet_meta['Site ID'] == site_ID, 'Latitude (degrees)'].values
-site_lon = fluxnet_meta.loc[fluxnet_meta['Site ID'] == site_ID, 'Longitude (degrees)'].values
+site_lat = fluxnet_meta.loc[
+    fluxnet_meta["Site ID"] == site_ID, "Latitude (degrees)"
+].values
+site_lon = fluxnet_meta.loc[
+    fluxnet_meta["Site ID"] == site_ID, "Longitude (degrees)"
+].values
 
-sel_file = get_single_match(amer_filepath + 'AMF_' + site_ID + '_FLUXNET_SUBSET_*/AMF_' + site_ID + '_FLUXNET_SUBSET_' + timedelta + '_*.csv')
+sel_file = get_single_match(
+    amer_filepath
+    + "AMF_"
+    + site_ID
+    + "_FLUXNET_SUBSET_*/AMF_"
+    + site_ID
+    + "_FLUXNET_SUBSET_"
+    + timedelta
+    + "_*.csv"
+)
 fluxnet_sel = pd.read_csv(sel_file)
-fluxnet_sel_sub = fluxnet_sel.loc[:,['TIMESTAMP','NEE_VUT_REF','NEE_VUT_REF_QC','GPP_NT_VUT_REF', 'GPP_DT_VUT_REF']].copy()
-fluxnet_sel_sub['TIMESTAMP'] = pd.to_datetime(fluxnet_sel_sub['TIMESTAMP'],format='%Y%m%d')
-fluxnet_sel_sub = fluxnet_sel_sub.set_index('TIMESTAMP')
+fluxnet_sel_sub = fluxnet_sel.loc[
+    :,
+    ["TIMESTAMP", "NEE_VUT_REF", "NEE_VUT_REF_QC", "GPP_NT_VUT_REF", "GPP_DT_VUT_REF"],
+].copy()
+fluxnet_sel_sub["TIMESTAMP"] = pd.to_datetime(
+    fluxnet_sel_sub["TIMESTAMP"], format="%Y%m%d"
+)
+fluxnet_sel_sub = fluxnet_sel_sub.set_index("TIMESTAMP")
 
 # Make a clean output df
 fluxnet_final = pd.DataFrame()
@@ -104,53 +130,67 @@ fluxnet_final = pd.DataFrame()
 # NEE
 ## Convert units
 ## FluxNet NEE_VUT_REF in DD (gC m-2 d-1) to MiCASA (kgC m-2 s-1)
-fluxnet_final['NEE (kgC m-2 s-1)'] = fluxnet_sel_sub['NEE_VUT_REF']*1e-3/86400
+fluxnet_final["NEE (kgC m-2 s-1)"] = fluxnet_sel_sub["NEE_VUT_REF"] * 1e-3 / 86400
 
 # GPP
 ## FluxNet GPP in DD (gC m-2 d-1) to MiCASA (kgC m-2 s-1)
-fluxnet_final['GPP (DT) (kgC m-2 s-1)'] = fluxnet_sel_sub['GPP_DT_VUT_REF']*1e-3/86400
-fluxnet_final['GPP (NT) (kgC m-2 s-1)'] = fluxnet_sel_sub['GPP_NT_VUT_REF']*1e-3/86400
+fluxnet_final["GPP (DT) (kgC m-2 s-1)"] = (
+    fluxnet_sel_sub["GPP_DT_VUT_REF"] * 1e-3 / 86400
+)
+fluxnet_final["GPP (NT) (kgC m-2 s-1)"] = (
+    fluxnet_sel_sub["GPP_NT_VUT_REF"] * 1e-3 / 86400
+)
 
 ## Mask bad QC values for NEE and GPP
 ## for daily FluxNet data, QC is fraction between 0-1, indicating percentage of measured and good quality gapfill data
-fluxnet_final['NEE (kgC m-2 s-1)'] = fluxnet_final['NEE (kgC m-2 s-1)'].mask(fluxnet_sel_sub['NEE_VUT_REF_QC'] < 1, np.nan)
-fluxnet_final['GPP (DT) (kgC m-2 s-1)'] = fluxnet_final['GPP (DT) (kgC m-2 s-1)'].mask(fluxnet_sel_sub['NEE_VUT_REF_QC'] < 1, np.nan)
+fluxnet_final["NEE (kgC m-2 s-1)"] = fluxnet_final["NEE (kgC m-2 s-1)"].mask(
+    fluxnet_sel_sub["NEE_VUT_REF_QC"] < 1, np.nan
+)
+fluxnet_final["GPP (DT) (kgC m-2 s-1)"] = fluxnet_final["GPP (DT) (kgC m-2 s-1)"].mask(
+    fluxnet_sel_sub["NEE_VUT_REF_QC"] < 1, np.nan
+)
 
 # Mask GPP outliers
-fluxnet_final = replace_outliers_with_nan(fluxnet_final,'GPP (DT) (kgC m-2 s-1)')
+fluxnet_final = replace_outliers_with_nan(fluxnet_final, "GPP (DT) (kgC m-2 s-1)")
 
 ############ Import Preprocessed Micasa Data ################
-filename = f'{site_ID}_micasa_{timedelta}.csv'
+filename = f"{site_ID}_micasa_{timedelta}.csv"
 path = os.path.join(mic_filepath, filename)
-micasa_ds = pd.read_csv(path,index_col=0, parse_dates=True)
+micasa_ds = pd.read_csv(path, index_col=0, parse_dates=True)
 
 ############## Append datasets #########################
 # Make clean dataframe and append together
 ## NEE
 NEE_ds = pd.DataFrame()
-NEE_ds['MiCASA'] = micasa_ds['MiCASA NEE (kg m-2 s-1)']
-NEE_ds['FluxNet'] = fluxnet_final['NEE (kgC m-2 s-1)']
+NEE_ds["MiCASA"] = micasa_ds["MiCASA NEE (kg m-2 s-1)"]
+NEE_ds["FluxNet"] = fluxnet_final["NEE (kgC m-2 s-1)"]
 
 ## NPP
 NPP_ds = pd.DataFrame()
-NPP_ds['MiCASA'] = micasa_ds['MiCASA NPP (kg m-2 s-1)']
-NPP_ds['FluxNet DT GPP/2'] = fluxnet_final['GPP (DT) (kgC m-2 s-1)']/2
+NPP_ds["MiCASA"] = micasa_ds["MiCASA NPP (kg m-2 s-1)"]
+NPP_ds["FluxNet DT GPP/2"] = fluxnet_final["GPP (DT) (kgC m-2 s-1)"] / 2
 
 
 ######### Create plots ########################
 # Create a subplot grid with specific width ratios
-fig, axs = plt.subplots(4, 1, 
-                         # subplot_kw={'projection': proj}, 
-                         gridspec_kw={'height_ratios': [1.2, 2,0.25,2],
-                                      'hspace': 0.01},
-                         figsize=(10, 12)) 
+fig, axs = plt.subplots(
+    4,
+    1,
+    # subplot_kw={'projection': proj},
+    gridspec_kw={"height_ratios": [1.2, 2, 0.25, 2], "hspace": 0.01},
+    figsize=(10, 12),
+)
 
 # Define the map projection
 proj = ccrs.PlateCarree()
 
 # Extract site lat/lon
-site_lat = ameriflux_meta.loc[ameriflux_meta['Site ID'] == site_ID, 'Latitude (degrees)'].values
-site_lon = ameriflux_meta.loc[ameriflux_meta['Site ID'] == site_ID, 'Longitude (degrees)'].values
+site_lat = ameriflux_meta.loc[
+    ameriflux_meta["Site ID"] == site_ID, "Latitude (degrees)"
+].values
+site_lon = ameriflux_meta.loc[
+    ameriflux_meta["Site ID"] == site_ID, "Longitude (degrees)"
+].values
 
 # Pick map location based on location of site
 if site_lat >= 20:
@@ -163,38 +203,35 @@ else:
     min_lon, max_lon = -90, -30
     min_lat, max_lat = -60, 12
 
-axs[0].axis('off')
-axs[0] = plt.subplot(4, 1, 1, projection=proj,frameon=False)
+axs[0].axis("off")
+axs[0] = plt.subplot(4, 1, 1, projection=proj, frameon=False)
 axs[0].set_extent([min_lon, max_lon, min_lat, max_lat], crs=ccrs.PlateCarree())
 axs[0].coastlines()
-axs[0].scatter(site_lon,site_lat,
-       marker='*', 
-       s=300,
-       color='yellow',
-       edgecolor='black',
-               zorder=3)
+axs[0].scatter(
+    site_lon, site_lat, marker="*", s=300, color="yellow", edgecolor="black", zorder=3
+)
 
 
-NEE_ds.plot(ax=axs[1],ylabel = 'NEE\n(kgC m$^{-2}$ s$^{-1}$)')
+NEE_ds.plot(ax=axs[1], ylabel="NEE\n(kgC m$^{-2}$ s$^{-1}$)")
 # Format x-axis labels
 axs[1].xaxis.set_major_locator(mdates.AutoDateLocator())
 # Disable minor ticks completely
-axs[1].tick_params(axis='x', which='minor', labelsize=0, labelcolor='none')
+axs[1].tick_params(axis="x", which="minor", labelsize=0, labelcolor="none")
 
 axs[2].set_visible(False)
 
-NPP_ds.plot(ax=axs[3],ylabel = 'NPP\n(kgC m$^{-2}$ s$^{-1}$)')
+NPP_ds.plot(ax=axs[3], ylabel="NPP\n(kgC m$^{-2}$ s$^{-1}$)")
 # Format x-axis labels
 axs[3].xaxis.set_major_locator(mdates.AutoDateLocator())
 # Disable minor ticks completely
-axs[3].tick_params(axis='x', which='minor', labelsize=0, labelcolor='none')
+axs[3].tick_params(axis="x", which="minor", labelsize=0, labelcolor="none")
 
 
-date_format = mdates.DateFormatter('%b %Y')
-for i in range(1,4,2):
+date_format = mdates.DateFormatter("%b %Y")
+for i in range(1, 4, 2):
     axs[i].xaxis.set_major_formatter(date_format)
-    axs[i].set_xlabel('') 
-fig.suptitle(f'{site_ID}',y=0.9,fontsize=14)
+    axs[i].set_xlabel("")
+fig.suptitle(f"{site_ID}", y=0.9, fontsize=14)
 
 output_path = os.path.join(output_dir, output_filename)
 
--- preprocessing/mp-controller-preprocessing.py
+++ preprocessing/mp-controller-preprocessing.py
@@ -4,6 +4,7 @@
 # Import config variables
 import sys
 from pathlib import Path
+
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from config import FLUX_METADATA
 
@@ -13,39 +14,47 @@ import subprocess
 import multiprocessing
 from functools import partial
 
-def run_py_script(script, arg): 
-    '''
+
+def run_py_script(script, arg):
+    """
     This function runs a python script with one argument via subprocess
     and returns a subprocess error if it occurs
-    '''
+    """
     cmd = ["python", script, arg]
     try:
-        result = subprocess.run(cmd, capture_output=True, text=True, check=True) # check=True will crash if error
+        result = subprocess.run(
+            cmd, capture_output=True, text=True, check=True
+        )  # check=True will crash if error
         print(result.stdout.strip())
-        return result.stdout.strip() 
+        return result.stdout.strip()
     except subprocess.CalledProcessError as e:
         print(f"Error running command: {' '.join(cmd)}")
         print("STDOUT:", e.stdout)
         print("STDERR:", e.stderr)
         return f"FAILED: {arg}"
 
-def run_multiprocessing(script, arg_list):
-    '''
-    This function runs run_py_script over a list of input arguments via multiprocessing
-    '''
-    run_script_with_arg = partial(run_py_script, script) # create a partial function to input script to
 
-    with multiprocessing.Pool() as pool: # should be fine to not specify pool size as long as no mem issues
-        results = pool.map(run_script_with_arg,arg_list) 
+def run_multiprocessing(script, arg_list):
+    """
+    This function runs run_py_script over a list of input arguments via multiprocessing
+    """
+    run_script_with_arg = partial(
+        run_py_script, script
+    )  # create a partial function to input script to
+
+    with (
+        multiprocessing.Pool() as pool
+    ):  # should be fine to not specify pool size as long as no mem issues
+        results = pool.map(run_script_with_arg, arg_list)
     return results
 
 
-if __name__ == "__main__": # Guard preventing future import issues
+if __name__ == "__main__":  # Guard preventing future import issues
     # Import/format the list of paths (for large data, put inside the guard)
-    ameriflux_meta = pd.read_csv(FLUX_METADATA, sep='\t')
-    fluxnet_meta = ameriflux_meta.loc[ameriflux_meta['AmeriFlux FLUXNET Data'] == 'Yes']
-    fluxnet_list = fluxnet_meta['Site ID'].to_list()
-    
+    ameriflux_meta = pd.read_csv(FLUX_METADATA, sep="\t")
+    fluxnet_meta = ameriflux_meta.loc[ameriflux_meta["AmeriFlux FLUXNET Data"] == "Yes"]
+    fluxnet_list = fluxnet_meta["Site ID"].to_list()
+
     script = "data-preprocessing.py"
 
     # Initialize run
