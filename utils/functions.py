9e3fcd293ef378af40841fd44b1b5c84fb719ab3
Hannah Zafar <hannah.zafar@nasa.gov>
2025-08-28 17:00:55 -0400

    Move nan function to utils/functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import glob
+import numpy as np
 
 
 def get_single_match(base_path, pattern):
@@ -30,3 +31,27 @@ def get_single_match(base_path, pattern):
         raise ValueError(f"No matches found for pattern '{pattern}' in {base_path}")
     else:
         raise ValueError(f"Multiple matches found for pattern '{pattern}': {matches}")
+
+
+def replace_outliers_with_nan(df, column):
+    """Replaces outliers (1.5 IQR above/below) in a DataFrame column with NaN.
+
+    Args:
+        df (pd.DataFrame): The DataFrame.
+        column (str): The column name to check for outliers.
+
+    Returns:
+        pd.DataFrame: The DataFrame with outliers replaced by NaN.
+    """
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    df[column] = df[column].mask(
+        (df[column] < lower_bound) | (df[column] > upper_bound), np.nan
+    )
+    return df
+
