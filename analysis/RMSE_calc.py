bcfdc156da20b24828bd4cbefbc315244ae66ec3
Hannah Zafar <hannah.zafar@nasa.gov>
2025-09-18 16:05:45 -0400

    Fix RMSE typo

--- analysis/RMSE_calc.py
+++ analysis/RMSE_calc.py
@@ -40,7 +40,7 @@ for site_ID in ids_list:
     NEE_ds["FluxNet"] = fluxnet_data["NEE (kgC m-2 s-1)"]
     # Drop NA values for RMSE
     NEE_ds_clean = NEE_ds.dropna(subset=['FluxNet'])
-    NEE_RSME = root_mean_squared_error(NEE_ds_clean.MiCASA, NEE_ds_clean.FluxNet)
+    NEE_RMSE = root_mean_squared_error(NEE_ds_clean.MiCASA, NEE_ds_clean.FluxNet)
 
     ## NPP
     NPP_ds = pd.DataFrame()
@@ -48,13 +48,13 @@ for site_ID in ids_list:
     NPP_ds["FluxNet DT GPP/2"] = fluxnet_data["GPP_DT (kgC m-2 s-1)"] / 2
     # Drop NA values for RMSE
     NPP_ds_clean = NPP_ds.dropna(subset=['FluxNet DT GPP/2'])
-    NPP_RSME = root_mean_squared_error(NPP_ds_clean.MiCASA, NPP_ds_clean["FluxNet DT GPP/2"])
+    NPP_RMSE = root_mean_squared_error(NPP_ds_clean.MiCASA, NPP_ds_clean["FluxNet DT GPP/2"])
 
     # Write values out to a list
     results.append({
         'SiteID' : site_ID,
-        'NEE_RSME': NEE_RSME,
-        'NPP_RSME': NPP_RSME,
+        'NEE_RMSE': NEE_RMSE,
+        'NPP_RMSE': NPP_RMSE,
     })
 
 ds = pd.DataFrame(results)
