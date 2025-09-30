19d22724c6a96cc03cd11be3abb7c84d902bfa4c
Hannah Zafar <hannah.zafar@nasa.gov>
2025-09-30 11:57:55 -0400

    Create universal function for importing RMSE data

--- utils/functions.py
+++ utils/functions.py
@@ -26,6 +26,23 @@ def import_flux_metadata(flux_metadata_path):
             ]  # use FLUXNET only
     return fluxnet_meta
 
+def import_site_RSME_data(flux_metadata_path,RSME_results_path):
+    fluxnet_meta = import_flux_metadata(flux_metadata_path)
+    site_subset = ['Site ID', 
+               'Longitude (degrees)',
+                'Latitude (degrees)',
+               ]
+    site_subset = ['Site ID', 
+               'Longitude (degrees)',
+                'Latitude (degrees)',
+               ]
+    df_meta = fluxnet_meta[site_subset]
+    df_meta = df_meta.set_index('Site ID')
+    df_meta = df_meta.rename(columns={'Latitude (degrees)': 'lat', 'Longitude (degrees)': 'lon'})
+
+    RMSE_results = pd.read_csv(RSME_results_path, index_col='SiteID')
+    return df_meta.join(RMSE_results, on='Site ID', how="inner")
+
 def local_std_to_utc_std(df, col, lat, lon):
     """ Convert local standard time (no DLS) to UTC
 
